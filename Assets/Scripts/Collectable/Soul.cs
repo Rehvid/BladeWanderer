908b3d14c88afa0ff5fbd7abaaaa148608c29ccd
Dawid Harendarz <dharendarz2@gmail.com>
2024-10-21 16:54:38 +0200

    Add soul VFX

--- Assets/Scripts/Collectable/Soul.cs
+++ Assets/Scripts/Collectable/Soul.cs
@@ -1,16 +1,26 @@
 ﻿namespace RehvidGames.Collectable
 {
+    using System;
     using Player;
     using UnityEngine;
     
     public class Soul: Collectable
     {
         [SerializeField] private int _amount = 5;
+        [SerializeField] private float _lifetime = 10f;
         
         public override void Collect(Player player)
         {
             player.Attributes.AddSouls(_amount);
             Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            if (Time.time >= _lifetime)
+            {
+                Debug.LogWarning("Soul - Remove"); //TODO: Jeśli minęło połowe czasu to zmniejsz size
+            }   
         }
     }
 }
