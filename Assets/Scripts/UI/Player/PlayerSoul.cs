3effea666383b340dc918ce329cfd1a9dcf0231c
Dawid Harendarz <dharendarz2@gmail.com>
2024-11-05 17:29:32 +0100

    Add animation text for Soul Counter

--- Assets/Scripts/UI/Player/PlayerSoul.cs
+++ Assets/Scripts/UI/Player/PlayerSoul.cs
@@ -1,15 +1,34 @@
 ﻿namespace RehvidGames.UI.Player
 {
+    using System.Collections;
     using TMPro;
     using UnityEngine;
 
     public class PlayerSoul: MonoBehaviour
     {
-        [SerializeField] private TMP_Text _text;
+        [SerializeField] private TextMeshProUGUI _textMeshPro;
+        [SerializeField] private float _animationDuration = 2f;
 
         public void OnUpdateText(int souls)
         {
-            _text.SetText(souls.ToString());
+            int currentValue = int.TryParse(_textMeshPro.text, out int parsedValue) ? parsedValue : 0;
+            StartCoroutine(AnimateTextCoroutine(currentValue, souls));
+        }
+
+        private IEnumerator AnimateTextCoroutine(int startValue, int endValue)
+        {
+            float elapsedTime = 0f;
+
+            while (elapsedTime < _animationDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                float t = elapsedTime / _animationDuration;
+                int currentValue = Mathf.RoundToInt(Mathf.Lerp(startValue, endValue, t));
+
+                _textMeshPro.text = currentValue.ToString();
+                yield return null;
+            }
+            _textMeshPro.text = endValue.ToString();
         }
     }
 }
