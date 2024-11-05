e656fec88c3bb431138f6d796178b537c4f1e1d2
Dawid Harendarz <dharendarz2@gmail.com>
2024-11-05 17:26:08 +0100

    Add stamina costs for player

---
+++ Assets/Scripts/ScriptableObjects/StaminaCosts.cs
@@ -0,0 +1,13 @@
+﻿namespace RehvidGames.ScriptableObjects
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "StaminaCosts", menuName = "Player/StaminaCosts")]
+    public class StaminaCosts : ScriptableObject
+    {
+        [Tooltip("Stamina cost of the running is multiplied by DeltaTime.")]
+        public float Run = 5f;
+        public float Jump = 10f;
+        public float Dodge = 10f;
+    }
+}
