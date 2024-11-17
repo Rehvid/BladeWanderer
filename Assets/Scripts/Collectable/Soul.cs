de4a3b169eded6562ca87d512edbbd3cb22f3578
Dawid Harendarz <dharendarz2@gmail.com>
2024-11-17 16:03:29 +0100

    Create treasure and soul inherit form that base class

--- Assets/Scripts/Collectable/Soul.cs
+++ Assets/Scripts/Collectable/Soul.cs
@@ -1,26 +1,13 @@
 ﻿namespace RehvidGames.Collectable
 {
-    using System;
     using Player;
-    using UnityEngine;
     
-    public class Soul: Collectable
+    public class Soul: Treasure
     {
-        [SerializeField] private int _amount = 5;
-        [SerializeField] private float _lifetime = 10f;
-        
         public override void Collect(Player player)
         {
-            player.Attributes.AddSouls(_amount);
+            player.Attributes.AddSouls(GetTreasureAmount());
             Destroy(gameObject);
-        }
-
-        private void Update()
-        {
-            if (Time.time >= _lifetime)
-            {
-                Debug.LogWarning("Soul - Remove"); //TODO: Jeśli minęło połowe czasu to zmniejsz size
-            }   
         }
     }
 }
