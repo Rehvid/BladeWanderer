02cf8a23245293c4e5ae14602aebf81b62251956
Dawid Harendarz <dharendarz2@gmail.com>
2024-11-25 04:42:45 +0100

    Create basic main menu

---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿namespace RehvidGames.UI.Menu
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class MainMenu : MonoBehaviour
+    {
+        #region Events
+        public void OnPlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void OnExitGame()
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+        #endregion
+    }
+}
