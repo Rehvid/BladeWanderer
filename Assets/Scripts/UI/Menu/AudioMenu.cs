0835a6a468f3f04ca7edcb83579960c1fb2a3d34
Dawid Harendarz <dharendarz2@gmail.com>
2024-11-26 04:01:44 +0100

    Add menu for configuring audio settings

---
+++ Assets/Scripts/UI/Menu/AudioMenu.cs
@@ -0,0 +1,25 @@
+﻿namespace RehvidGames.UI.Menu
+{
+    using UnityEngine;
+    using UnityEngine.Audio;
+
+    public class AudioMenu : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer _audioMixer;
+
+        public void OnMasterVolume(float value)
+        {
+            _audioMixer.SetFloat("MasterVolume", value);
+        }
+
+        public void OnMusicVolume(float value)
+        {
+            _audioMixer.SetFloat("Music", value);
+        }
+
+        public void OnSFXVolume(float value)
+        {
+            _audioMixer.SetFloat("SFX", value);
+        }
+    }
+}
