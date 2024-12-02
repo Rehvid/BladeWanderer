bf23f0a170fbf0000c4d7a27dd2bc539aa459f36
Dawid Harendarz <dharendarz2@gmail.com>
2024-12-02 05:27:19 +0100

    Add forgotten file in the previous commit

---
+++ Assets/Scripts/DataPersistence/Managers/AutoSaveManager.cs
@@ -0,0 +1,50 @@
+﻿namespace RehvidGames.DataPersistence.Managers
+{
+    using System.Collections;
+    using Helpers;
+    using UnityEngine;
+
+    public class AutoSaveManager: MonoBehaviour
+    {
+        private bool _isAutoSaveEnabled = true;
+        
+        [Header("Auto Saving Configuration")]  
+        [SerializeField] private float _autoSaveTimeSeconds = 360f;
+        
+        private SaveLoadManager _saveLoadManager;
+        private Coroutine _autoSaveCoroutine;
+
+        public void SetSaveLoadService(SaveLoadManager saveLoadManager)
+        {
+            _saveLoadManager = saveLoadManager;
+        }
+
+        public void HandleAutoSaveCoroutine()
+        {
+            if (_autoSaveCoroutine != null)
+            {
+                StopCoroutine(_autoSaveCoroutine);
+            }
+            _autoSaveCoroutine = StartCoroutine(AutoSave());
+        }
+
+        public void StopAutoSaveCoroutine()
+        {
+            _isAutoSaveEnabled = false;
+            if (_autoSaveCoroutine == null) return;
+            
+            StopCoroutine(_autoSaveCoroutine);
+            _autoSaveCoroutine = null;
+        }
+
+        private IEnumerator AutoSave()
+        {
+            while (_isAutoSaveEnabled)
+            {
+                yield return new WaitForSeconds(_autoSaveTimeSeconds);
+                _saveLoadManager.SaveGame(PersistenceObjectFinder.FindAll());
+                Debug.Log("Auto saving...");
+            }
+        }
+    }
+}
