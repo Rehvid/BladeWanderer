612fcc43595193a297cc9b4309783a862278f131
Dawid Harendarz <dharendarz2@gmail.com>
2024-12-03 04:43:49 +0100

    Add default values for GameplaySettings

--- Assets/Scripts/DataPersistence/Data/GameGameplaySettings.cs
+++ Assets/Scripts/DataPersistence/Data/GameGameplaySettings.cs
@@ -1,10 +1,23 @@
 ﻿namespace RehvidGames.DataPersistence.Data
 {
+    using System.Linq;
+    using UnityEngine;
+
     [System.Serializable]
     public class GameGameplaySettings
     {
-        public bool IsFullScreen;
+        private const int HighGraphicQualityIndex = 0;
+        
+        public bool IsFullScreen = true;
         public int ResolutionIndex;
-        public int QualityIndex;
+        public int QualityIndex = HighGraphicQualityIndex;
+
+        public GameGameplaySettings()
+        {
+            ResolutionIndex = Screen.resolutions
+                .Select((resolution, index) => new { resolution, index })
+                .OrderByDescending(res => res.resolution.width * res.resolution.height)
+                .FirstOrDefault()?.index ?? 0;
+        }
     }
 }
