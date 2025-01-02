4534c2851b5fc7d0316b94a93e4e7829b3c16c0e
Dawid Harendarz <dharendarz2@gmail.com>
2025-01-02 04:27:19 +0100

    Split PlayerMovement to another class for better readability

---
+++ Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -0,0 +1,206 @@
+﻿namespace RehvidGames.Characters.Player
+{
+    using Animator;
+    using Audio;
+    using Enums;
+    using Managers;
+    using UnityEngine;
+    using UnityEngine.InputSystem;
+    using VFXManager = VFX.VFXManager;
+
+    //TODO: Refactor later
+    public class PlayerMovement: MonoBehaviour
+    {
+        private const float MinimumRunningSpeed = 1f;
+        
+        [Header("Configuration")]
+        [SerializeField] private PlayerMovementHandler _movementHandler;
+        [SerializeField] private bool _isLockingCamera;
+        
+        [Header("Speed parameters")] 
+        [Tooltip("Walking speed of the character.")] 
+        [SerializeField] private float _walkSpeed = 5f;
+        
+        [Tooltip("Running speed of the character.")] 
+        [SerializeField] private float _runSpeed = 10f;
+        
+        [Header("Animator parameters")]
+        [Tooltip("Constant Y value used for vertical movement in the animator.")]
+        [SerializeField] private float _animatorVerticalOffset = -0.5f;
+        [Tooltip("Smoothing factor for speed transitions in the animator. Lower values make animations more responsive to speed changes, while higher values create smoother, slower adjustments.")]
+        [SerializeField] private float _animatorSpeedSmoothingFactor = 0.1f;
+        
+        private bool _isStopped;
+        private bool _canPlayDustGroundVFX;
+        private bool _isFootstepsSoundPlaying;
+        private float _previousSpeed;
+         
+        private PlayerController _player;
+        private AnimatorHandler _animatorHandler;
+        private AudioManager _audioManager;
+        
+        private void Start()
+        {
+            _player = GameManager.Instance.Player;
+            if (_player == null)
+            {
+                Debug.LogWarning($"Player not found in");
+            }
+            
+            _animatorHandler = _player.AnimatorHandler;
+            if (_isLockingCamera)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            
+            _audioManager = AudioManager.Instance;
+        }
+
+        private void Update()
+        {
+            if (GameManager.Instance.IsPaused) return;
+            
+            HandlePlayerMovement();
+            if (!_isStopped)
+            {
+                UpdateAnimatorParameters();
+            }
+            
+            PlayMovementStopEffects();
+            PlayMovementSound();
+        }
+
+        private void HandlePlayerMovement()
+        {
+            _movementHandler.HandleRunning(_walkSpeed);
+            
+            if (_movementHandler.IsPlayerInAction())
+            {
+                StopPlayerMovementInAction();
+                return;
+            }
+
+            if (!_movementHandler.CanMove())
+            {
+                _movementHandler.DecelerateToStop(_animatorVerticalOffset);
+                return;
+            }
+            
+            _canPlayDustGroundVFX = true;
+            _movementHandler.HandleMovement();
+        }
+
+        private void StopPlayerMovementInAction()
+        {
+            _animatorHandler.SetFloat(AnimatorParameter.XSpeed, 0);
+            if (_isFootstepsSoundPlaying)
+            {
+                _audioManager.StopCurrentSoundType(SoundType.PlayerFootsteps);
+            }
+            _isFootstepsSoundPlaying = false;
+        }
+      
+        private void UpdateAnimatorParameters()
+        {
+            float interpolatedSpeed = CalculateInterpolatedSpeed();
+            
+            _animatorHandler.SetFloat(AnimatorParameter.XSpeed, interpolatedSpeed);
+            _animatorHandler.SetFloat(AnimatorParameter.YSpeed, _animatorVerticalOffset);
+            _previousSpeed = interpolatedSpeed;
+            
+            HandleMovementStop(interpolatedSpeed);
+        }
+
+        private float CalculateInterpolatedSpeed()
+        {
+            float currentSpeed = new Vector3(
+                _movementHandler.CharacterController.velocity.x, 
+                0, 
+                _movementHandler.CharacterController.velocity.z
+            ).magnitude;
+            
+            return Mathf.Lerp(_previousSpeed, currentSpeed,  _animatorSpeedSmoothingFactor);
+        }
+
+        private void HandleMovementStop(float interpolatedSpeed)
+        {
+            if (!(interpolatedSpeed <= Mathf.Epsilon)) return;
+            
+            _isStopped = true;
+            _movementHandler.CurrentSpeed = 0;
+        }
+
+        private void PlayMovementStopEffects()
+        {
+            if (!ShouldPlayDustGroundVFX()) return;
+            
+            VFXManager.Instance.PlayParticleEffect(_player.CharacterEffectsData.DustGroundVFX, transform.position);
+            _canPlayDustGroundVFX = false;
+        }
+
+        private bool ShouldPlayDustGroundVFX() => !_movementHandler.CanMove() && _canPlayDustGroundVFX;
+
+        private void PlayMovementSound()
+        {
+            if (!_movementHandler.CanMove())
+            {
+                if (_isFootstepsSoundPlaying)
+                {
+                    StopMovementSound();
+                }
+                return;
+            }
+            
+            if (IsFootstepsSoundPlaying())
+            {
+                _audioManager.PlayRandomClip(SoundType.PlayerFootsteps);
+            }
+            
+            if (_isFootstepsSoundPlaying) return;
+            
+            _audioManager.PlayRandomClip(SoundType.PlayerFootsteps);
+            _isFootstepsSoundPlaying = true;
+        }
+        
+        private bool IsFootstepsSoundPlaying()
+        {
+            var currentContext = _audioManager.GetCurrentAudioPlayContext(SoundType.PlayerFootsteps);
+            
+            return _isFootstepsSoundPlaying && currentContext?.SoundType != SoundType.PlayerFootsteps;
+        }
+
+        private void StopMovementSound()
+        {
+            _audioManager.StopCurrentSoundType(SoundType.PlayerFootsteps);
+            _isFootstepsSoundPlaying = false;
+        }
+        
+        #region Input events
+        public void OnMovePerformed(InputAction.CallbackContext context)
+        {
+            _isStopped = false;
+            var inputMovement = context.ReadValue<Vector2>();
+            
+            _movementHandler.MovementInput = new Vector3(inputMovement.x, 0f, inputMovement.y).normalized;
+            
+            if (!_movementHandler.IsRunHolding || _movementHandler.CurrentSpeed < MinimumRunningSpeed)
+            {
+                _movementHandler.CurrentSpeed = _walkSpeed; 
+            }
+        }
+        
+        public void OnRunPerformed(InputAction.CallbackContext context)
+        {
+            if (_movementHandler.ShouldStopRunning) return;
+            
+            if (_player.IsRegenerationStarted())
+            {
+                _player.StopRegenerationStamina();
+            }
+            
+            _movementHandler.IsRunHolding = !context.canceled;
+            _movementHandler.StartSpeedCoroutine(context.canceled ? _walkSpeed : _runSpeed);
+        }
+        #endregion
+    }
+}
--- Assets/Scripts/Characters/Player/PlayerMovementHandler.cs
+++ Assets/Scripts/Characters/Player/PlayerMovementHandler.cs
@@ -1,29 +1,17 @@
 ﻿namespace RehvidGames.Characters.Player
 {
     using System.Collections;
-    using Animator;
     using Audio;
-    using Enums;
     using Managers;
     using UnityEngine;
     using UnityEngine.InputSystem;
-    using VFXManager = VFX.VFXManager;
 
-    //TODO: Refactor later
     public class PlayerMovementHandler: MonoBehaviour
     {
+        private const float MinMovementThreshold = 0.1f;
         
         [Header("Configuration")]
         [SerializeField] private CharacterController _characterController;
-        
-        private PlayerController _player;
-        
-        [Header("Speed parameters")] 
-        [Tooltip("Walking speed of the character.")] 
-        [SerializeField] private float _walkSpeed = 5f;
-
-        [Tooltip("Running speed of the character.")] 
-        [SerializeField] private float _runSpeed = 10f;
         [Tooltip("Rate at which the character decelerates to a full stop when movement input is released. Higher values cause quicker stops, while lower values result in a slower, more gradual deceleration.")]
         [SerializeField] private float _decelerationRate = 5f;
         
@@ -32,106 +20,70 @@
         [SerializeField] private Transform _cameraTransform;
         [Tooltip("Time taken to smoothly rotate the character towards the movement direction.")] 
         [SerializeField] private float  _rotationSmoothingTime = 0.1f;
-        [SerializeField] private bool _isLockingCamera;
         
-        [Header("Animator parameters")]
-        [Tooltip("Constant Y value used for vertical movement in the animator.")]
-        [SerializeField] private float _animatorVerticalOffset = -0.5f;
-        [Tooltip("Smoothing factor for speed transitions in the animator. Lower values make animations more responsive to speed changes, while higher values create smoother, slower adjustments.")]
-        [SerializeField] private float _animatorSpeedSmoothingFactor = 0.1f;
-
-        private bool _stopRunning;
-        private bool _isRunHolding;
-        private bool _isStopped;
-        private bool _canInvokeVfxEffects;
-        private bool _isPlayerFootstepsSoundPlaying;
-        private bool _isGamePaused;
+        public CharacterController CharacterController => _characterController;
         
-        private float _fullRunStaminaCost => _player.StaminaCostsData.BaseRun * Time.deltaTime;
-        private float _rotationVelocity;
-        private float _currentSpeed;
-        private float _previousSpeed;
+        public bool IsRunHolding { get; set; }
         
-        private Vector3 _movementInput;
-        private Coroutine _speedCoroutine;
-        private AnimatorHandler _animatorHandler;
+        public bool ShouldStopRunning { get; private set; }
+        
+        public Vector3 MovementInput { get; set; }
+        
+        public float CurrentSpeed { get; set; }
+        
+        private PlayerController _player;
         private AudioManager _audioManager;
+        private Coroutine _speedCoroutine;
+        
+        private float _rotationVelocity;
         
         private void Start()
+        {
+            FindComponents();
+        }
+
+        private void FindComponents()
         {
             _player = GameManager.Instance.Player;
             if (_player == null)
             {
-                Debug.LogWarning($"Player not found in");
+                Debug.LogWarning("Player not found in");
             }
-            
-            _animatorHandler = _player.AnimatorHandler;
-            if (_isLockingCamera)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-            }
-            
             _audioManager = AudioManager.Instance;
-        }
-
-        private void Update()
-        {
-            if (GameManager.Instance.IsPaused) return;
-            HandlePlayerRunning();
-            
-            if (!_player.ActionHandler.IsUnoccupied())
+            if (_audioManager == null)
             {
-                _animatorHandler.SetFloat(AnimatorParameter.XSpeed, 0);
-                if (_isPlayerFootstepsSoundPlaying)
-                {
-                      _audioManager.StopCurrentSoundType(SoundType.PlayerFootsteps);
-                }
-                _isPlayerFootstepsSoundPlaying = false;
-              
-                return; 
+                Debug.LogWarning("Audio manager not found in");
             }
-            HandleMovement();
-
-            if (!_isStopped)
-            {
-                UpdateAnimatorParameters();
-            }
-            
-            PlayMovementStopEffects();
-            PlayMovementSound();
         }
         
-        private void HandlePlayerRunning()
+        public bool IsPlayerInAction() => !_player.ActionHandler.IsUnoccupied();
+
+        private float RunStaminaCost() => _player.StaminaCostsData.BaseRun * Time.deltaTime;
+        
+        public void HandleRunning(float walkSpeed)
         {
-            if (!HasEnoughStaminaToRun())
+            if (!_player.HasEnoughStamina(RunStaminaCost()))
             {
-                StopRunning();
+                StopRunning(walkSpeed);
                 return;
             }
 
             ContinueRunning();
         }
         
-        private bool HasEnoughStaminaToRun() => _player.HasEnoughStamina(_fullRunStaminaCost);
-
-        private void StopRunning()
+        private void StopRunning(float walkSpeed)
         {
-            if (_currentSpeed > _walkSpeed)
+            if (CurrentSpeed > walkSpeed)
             { 
-                StartSpeedCoroutine(_walkSpeed);
-            }
-            _stopRunning = true;
-                
-            if (_player.IsRegenerationStarted())
-            {
-                _player.StopRegenerationStamina();
-                return;
+                StartSpeedCoroutine(walkSpeed);
             }
             
-            _player.RegenerationStamina();  
+            ShouldStopRunning = true;
+            
+            HandleStamina();
         }
         
-        private void StartSpeedCoroutine(float targetSpeed)
+        public void StartSpeedCoroutine(float targetSpeed)
         {
             if (_speedCoroutine != null)
             {
@@ -141,50 +93,55 @@
         }
         private IEnumerator SmoothSpeedTransition(float targetSpeed, float transitionTime)
         {
-            float initialSpeed = _currentSpeed;
-            float elapsedTime = 0f;
+            float initialSpeed = CurrentSpeed;
+            var elapsedTime = 0f;
         
             while (elapsedTime < transitionTime)
             {
                 elapsedTime += Time.deltaTime;
                 float time = elapsedTime / transitionTime;
-                _currentSpeed = Mathf.Lerp(initialSpeed, targetSpeed, time);
+                CurrentSpeed = Mathf.Lerp(initialSpeed, targetSpeed, time);
                 yield return null;
             }
         
-            _currentSpeed = targetSpeed;
+            CurrentSpeed = targetSpeed;
+        }
+
+        private void HandleStamina()
+        {
+            if (_player.IsRegenerationStarted())
+            {
+                _player.StopRegenerationStamina();
+                return;
+            }
+            
+            _player.RegenerationStamina(); 
         }
         
         private void ContinueRunning()
         {
-            _stopRunning = false;
-            if (_isRunHolding)
+            ShouldStopRunning = false;
+            if (IsRunHolding)
             {
-                _player.UseStamina(_fullRunStaminaCost);
+                _player.UseStamina(RunStaminaCost());
                 return;
             }
           
             _player.RegenerationStamina();
         }
-
-        private void HandleMovement()
+        
+        public void HandleMovement()
         {
-            if (!CanMove())
-            {
-                DecelerateToStop();
-                return;
-            }
-            _canInvokeVfxEffects = true;
             ApplyRotation();
             MoveCharacter();
         }
         
-        private bool CanMove() => _movementInput.magnitude >= 0.1f;
+        public bool CanMove() => MovementInput.magnitude >= MinMovementThreshold;
         
-        private void DecelerateToStop()
+        public void DecelerateToStop(float animatorVerticalOffset)
         {
-            _characterController.Move(new Vector3(0, _animatorVerticalOffset, 0));
-            _currentSpeed = Mathf.Lerp(_currentSpeed, 0f, Time.deltaTime * _decelerationRate);
+            _characterController.Move(new Vector3(0, animatorVerticalOffset, 0));
+            CurrentSpeed = Mathf.Lerp(CurrentSpeed, 0f, Time.deltaTime * _decelerationRate);
         }
         
         private void ApplyRotation()
@@ -205,15 +162,12 @@
         
         private float CalculateTargetRotationAngle()
         {
-            var angleXZ = Mathf.Atan2(_movementInput.x, _movementInput.z) * Mathf.Rad2Deg;
+            var angleXZ = Mathf.Atan2(MovementInput.x, MovementInput.z) * Mathf.Rad2Deg;
             return angleXZ + _cameraTransform.eulerAngles.y; 
         }
         
-        
-        private void RotateCharacterTo(float targetRotation)
-        {
-            _characterController.transform.rotation = Quaternion.Euler(0f, targetRotation, 0f);
-        }
+        private void RotateCharacterTo(float targetRotation) 
+            => _characterController.transform.rotation = Quaternion.Euler(0f, targetRotation, 0f);
         
         private void MoveCharacter()
         {
@@ -221,87 +175,8 @@
             _characterController.Move(normalizedMoveDirection);
         }
         
+        private Vector3 CalculateMoveDirection() => Quaternion.Euler(0f, CalculateTargetRotationAngle(), 0f) * Vector3.forward; 
         
-        private Vector3 CalculateMoveDirection()
-        {
-            return Quaternion.Euler(0f, CalculateTargetRotationAngle(), 0f) * Vector3.forward; 
-        }
-        
-        private Vector3 NormalizeMoveDirection(Vector3 moveDirection)
-        {
-            return moveDirection.normalized * (_currentSpeed * Time.deltaTime);
-        }
-        
-        private void UpdateAnimatorParameters()
-        {
-            float currentSpeed = new Vector3(_characterController.velocity.x, 0, _characterController.velocity.z).magnitude;
-            float interpolatedSpeed = Mathf.Lerp(_previousSpeed, currentSpeed,  _animatorSpeedSmoothingFactor);
-            
-            _animatorHandler.SetFloat(AnimatorParameter.XSpeed, interpolatedSpeed);
-            _animatorHandler.SetFloat(AnimatorParameter.YSpeed, _animatorVerticalOffset);
-            _previousSpeed = interpolatedSpeed;
-            
-            if (!(interpolatedSpeed <= 0.01)) return;
-            _isStopped = true;
-            _currentSpeed = 0;
-        }
-
-        private void PlayMovementStopEffects()
-        {
-            if (CanMove() || !_canInvokeVfxEffects) return;
-            
-            VFXManager.Instance.PlayParticleEffect(_player.CharacterEffectsData.DustGroundVFX, transform.position);
-            _canInvokeVfxEffects = false;
-        }
-
-        private void PlayMovementSound()
-        {
-            if (!CanMove())
-            {
-                if (_isPlayerFootstepsSoundPlaying)
-                {
-                    StopMovementSound();
-                }
-                return;
-            }
-            if (_isPlayerFootstepsSoundPlaying && _audioManager.GetCurrentAudioPlayContext(SoundType.PlayerFootsteps)?.SoundType !=  SoundType.PlayerFootsteps)
-            {
-                _audioManager.PlayRandomClip(SoundType.PlayerFootsteps);
-            }
-            
-            if (_isPlayerFootstepsSoundPlaying) return;
-            _audioManager.PlayRandomClip(SoundType.PlayerFootsteps);
-            _isPlayerFootstepsSoundPlaying = true;
-        }
-
-        private void StopMovementSound()
-        {
-            _audioManager.StopCurrentSoundType(SoundType.PlayerFootsteps);
-            _isPlayerFootstepsSoundPlaying = false;
-        }
-        
-        #region Input events
-        public void OnMovePerformed(InputAction.CallbackContext context)
-        {
-            _isStopped = false;
-            var inputMovement = context.ReadValue<Vector2>();
-            _movementInput = new Vector3(inputMovement.x, 0f, inputMovement.y).normalized;
-            if (!_isRunHolding || _currentSpeed < 1f)
-            {
-                _currentSpeed = _walkSpeed; 
-            }
-        }
-        
-        public void OnRunPerformed(InputAction.CallbackContext context)
-        {
-            if (_stopRunning) return;
-            if (_player.IsRegenerationStarted())
-            {
-                _player.StopRegenerationStamina();
-            }
-            _isRunHolding = !context.canceled;
-            StartSpeedCoroutine(context.canceled ? _walkSpeed : _runSpeed);
-        }
-        #endregion
+        private Vector3 NormalizeMoveDirection(Vector3 moveDirection) => moveDirection.normalized * (CurrentSpeed * Time.deltaTime);
     }
 }
